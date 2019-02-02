ec4db1aee2643b9350924cf10cf58f5b43199da1
Inyong Hwang <inyong1020@gmail.com>
2019-02-02 23:27:53 +0900

    Create JK_SERVO.ino

---
+++ Makerthon/JK THROLLING/JK_SERVO.ino
@@ -0,0 +1,36 @@
+// SERVO TEST
+
+// LIBRARY
+#include <Servo.h> 
+
+// DEFINE
+Servo MOON_LEFT;
+Servo MOON_RIGHT;
+int moon = 0;
+#define PIN_MOON_LEFT 11
+#define PIN_MOON_RIGHT 6
+
+// SETUP
+void setup() 
+{ 
+  MOON_LEFT.attach(PIN_MOON_LEFT);
+  MOON_RIGHT.attach(PIN_MOON_RIGHT);
+} 
+
+// LOOP
+void loop() 
+{ 
+  for(moon = 0; moon <= 90; moon += 1)
+  {
+    MOON_LEFT.write(moon);
+    MOON_RIGHT.write(90-moon);
+    delay(15);
+  }
+  
+  for(moon = 90; moon>=0; moon-=1)
+  {
+    MOON_LEFT.write(moon);
+    MOON_RIGHT.write(90-moon);
+    delay(15); 
+  }
+} 
